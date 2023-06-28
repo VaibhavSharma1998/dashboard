ed6cf6645c37d906f857d197ca39f3d47795a6a9
VaibhavSharma1998 <83278317+VaibhavSharma1998@users.noreply.github.com>
2023-06-28 17:49:01 +0530

    slide navbar completed

--- src/Components/Slide.jsx
+++ src/Components/Slide.jsx
@@ -1,54 +1,77 @@
-import React, { children } from 'react'
-import { FaHome,FaChartBar,FaSnapchat,FaDatabase} from "react-icons/fa";
-import { RxHamburgerMenu,RxCross2 } from "react-icons/rx";
+import React,{useState} from 'react'
+import { FaHome, FaChartBar, FaSnapchat, FaDatabase } from "react-icons/fa";
+import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
 import { NavLink } from 'react-router-dom';
 
 
-const Slide = ({children}) => {
+const Slide = ({ children }) => {
+    const[isopen,setIsOpen] = useState(false);
+    const[slideWidth,setSlideWidth] = useState('w-64')
+    const[logoDisplay,setLogoDisplay] = useState('block')
+     
+    const isToggle = ()=>{
+        setIsOpen(!isopen)
+        setSlideWidth(isopen?'w-64':'w-16')
+        // setSlideWidth(isOpen ? 'w-64' : 'w-16');
+        setLogoDisplay(isopen?'block':'hidden')
+    }
+   
+    
     const slideData = [
-       
+
         {
-            path:'/dashboard',
-            name:'Dashboard',
-            icon:<FaHome/>
+            path: '/dashboard',
+            name: 'Dashboard',
+            icon: <FaHome />
         },
         {
-            path:'/statistic',
-            name:'Statistic',
-            icon:<FaChartBar/>
+            path: '/statistic',
+            name: 'Statistic',
+            icon: <FaChartBar />
         },
         {
-            path:'/chat',
-            name:'ChatMangement',
-            icon:<FaSnapchat/>
+            path: '/chat',
+            name: 'Chat',
+            icon: <FaSnapchat />
         },
         {
-            path:'/settings',
-            name:'Settings',
-            icon:<FaDatabase/>
+            path: '/settings',
+            name: 'Settings',
+            icon: <FaDatabase />
         },
     ]
-  return (
-    <div className='bg-red-500 w-64 text-black h-screen'>
-        <div className="flex justify-between">
-            <h1 className="logo">Vaibhav's Dashboard</h1>
-            <div className="cursor-pointer">
-                <RxHamburgerMenu/>
+    
+    return (
+        <>
+            {/* container div */}
+            <div className="flex">
+                {/* Slider  div*/}
+                <div className={`bg-[#fff] ${slideWidth} text-black h-screen`}>
+                    <div className="flex items-center py-6 px-4">
+                        <h1 className={`pr-24 text-2xl ${logoDisplay}`}>Logo</h1>
+                        
+                        <div className="cursor-pointer text-2xl" onClick={isToggle}>
+                            {isopen?<RxHamburgerMenu/>:<RxCross2/>}
+                        </div>
+                    </div>
+
+                    {
+                        slideData.map((value, index) => (
+                            <NavLink to={value.path} key={index} activeClassName="active active:bg-[#f7f7f8] " className='flex items-center py-6 px-4 hover:bg-[#f7f7f8] '>
+                                <div className="pr-10 ">{value.icon}</div>
+                                <div className="link_name">{value.name}</div>
+                            </NavLink>
+                        ))
+                    }
+
+
+                </div>
+                    {/* Main */}
+                <main className='w-full p-5 flex bg-[#f7f7f8]'>{children}</main>
+
             </div>
-        </div>
-        
-        {
-            slideData.map((value,index)=>(
-                <NavLink to={value.path} key={index} activeClassName="active" className='flex justify-between'>
-                    <div className="icon">{value.icon}</div>
-                    <div className="link_name">{value.name}</div>
-                 </NavLink>   
-            ))
-        }
-        
-        <main className='w-full p-5'>{children}</main>
-    </div>
-  )
+        </>
+    )
 }
 
 export default Slide
