bc6dba1a795637217d6a733e517714b4838a43fd
VaibhavSharma1998 <83278317+VaibhavSharma1998@users.noreply.github.com>
2023-07-03 14:20:01 +0530

    Desiging of chatbot is completed

--- src/Components/ChatManagement.jsx
+++ src/Components/ChatManagement.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
 
 const ChatManagement = () => {
+  const chatData = [
+    {
+      Client: 'Hello',
+      AI: 'Hi'
+    },
+    {
+      Client: 'How are you?',
+      AI: 'I am good,Thanks for asking, what about you😊?'
+    },
+    {
+      Client: 'I am also good😉',
+      AI: 'Tell me,how may i help you'
+    },
+
+  ]
   return (
-    <div>
-      Chat management
-    </div>
+
+    <>
+      {/* Container -main div  that gives height and width*/}
+      <div className="flex justify-center  h-full">
+        {/* Box div in which all the data is available */}
+        <div className="w-[32rem] h-[38rem] bg-[#fff] rounded-xl">
+          {/* upper div conatining name and image */}
+          <div className="flex bg-[#4E2D79]">
+            <span className="bg-black w-9 h-9 rounded-full my-4 mx-8 "></span>
+            <p className='text-white py-5 text-lg'>Artifical Intelligent</p>
+          </div>
+          {/* Bottom div conatining chat  */}
+
+          {
+            chatData.map((value) => (
+              <>
+                <div className='flex justify-end'>
+                  <p className="bg-[#D3D3D3] text-right border-2  my-4 mx-4 inline-block rounded-lg py-2 px-2 text-lg">{value.Client}</p>
+                </div>
+                <div className='flex justify-start'>
+                  <p className="bg-[#4E2D79] text-white text-left border-2  mx-4 inline-block rounded-xl  py-2 px-2 text-lg">{value.AI}</p>
+                </div>
+
+              </>
+            ))
+          }
+
+
+
+
+
+
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Components/Slide.jsx
+++ src/Components/Slide.jsx
@@ -1,21 +1,20 @@
 import React,{useState} from 'react'
 import { FaHome, FaChartBar, FaSnapchat, FaDatabase } from "react-icons/fa";
 import { RxHamburgerMenu, RxCross2 } from "react-icons/rx";
-import { NavLink } from 'react-router-dom';
+import { NavLink,useLocation } from 'react-router-dom';
 
 
 const Slide = ({ children }) => {
     const[isopen,setIsOpen] = useState(false);
-    const[slideWidth,setSlideWidth] = useState('w-64')
-    const[logoDisplay,setLogoDisplay] = useState('block')
+   const location = useLocation()
+   console.log(location)
      
     const isToggle = ()=>{
         setIsOpen(!isopen)
-        setSlideWidth(isopen?'w-64':'w-16')
-        // setSlideWidth(isOpen ? 'w-64' : 'w-16');
-        setLogoDisplay(isopen?'block':'hidden')
+       
     }
    
+
     
     const slideData = [
 
@@ -46,28 +45,29 @@ const Slide = ({ children }) => {
             {/* container div */}
             <div className="flex">
                 {/* Slider  div*/}
-                <div className={`bg-[#fff] ${slideWidth} text-black  h-screen transition-all duration-500 `}>
+                <div className={`bg-[#fff] ${isopen?`w-64`:`w-16`} text-black  h-screen transition-all duration-500 `}>
                     <div className="flex items-center py-6 px-4">
-                        <h1 className={`pr-24 text-2xl ${logoDisplay}`}>Logo</h1>
+                        <h1 className={`pr-24 text-2xl ${isopen?`block`:`hidden`}`}>Logo</h1>
                         
                         <div className="cursor-pointer text-2xl" onClick={isToggle}>
-                            {isopen?<RxHamburgerMenu/>:<RxCross2/>}
+                            {isopen?<RxCross2/>:<RxHamburgerMenu/>}
                         </div>
                     </div>
 
                     {
                         slideData.map((value, index) => (
-                            <NavLink to={value.path} key={index} activeClassName="active active:bg-[#f7f7f8] " className='flex items-center py-6 px-4 hover:bg-[#f7f7f8] '>
+                            <NavLink to={value.path} key={index}  className={`flex items-center py-6 px-4 hover:bg-[#f7f7f8] ${location.pathname ===value.path ?`bg-[#f7f7f8]`:`bg-[#fff]`}`}>
                                 <div className="pr-10 ">{value.icon}</div>
                                 <div className="link_name">{value.name}</div>
                             </NavLink>
+
                         ))
+
                     }
-
-
+                    
                 </div>
                     {/* Main */}
-                <main className='w-full p-5 flex bg-[#f7f7f8] text-3xl'>{children}</main>
+                <main className='w-full p-5 bg-[#D3D3D3] text-3xl'>{children}</main>
 
             </div>
         </>
